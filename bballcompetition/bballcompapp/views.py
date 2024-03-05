ba255a7d746eba76203397c4e492328daf326af6
Bladi Duran <bladi.duran@fullyaccountable.com>
2024-03-05 14:58:51 -0500

    add initial template UI

--- bballcompetition/bballcompapp/views.py
+++ bballcompetition/bballcompapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    # Logic for your view goes here
+    # context = {
+    #     'message': 'Hello, World!'
+    # }
+    return render(request, 'bballcompapp/home.html')
--- bballcompetition/bballcompetition/settings.py
+++ bballcompetition/bballcompetition/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bballcompapp',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,18 @@ WSGI_APPLICATION = 'bballcompetition.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'bballproj',
+            'USER': 'root',
+            'PASSWORD': 'bballpass',
+            'HOST': 'localhost', 
+            'PORT': '32769',     
+        }
 }
 
 
